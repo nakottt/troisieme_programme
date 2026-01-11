47d58b06d7363a2b9451eed153529ae9c5ab4eb5
Nakottt <hugo.warnie@hotmail.com>
2026-01-11 16:58:27 +0100

    troisieme_programme v0.4

---
+++ core/combat.py
@@ -0,0 +1,39 @@
+import time
+from spells import poule_attaque
+from spells import classes_attackes
+
+
+
+def pve(nom_joueur, classe_joueur, hp_joueur, nom_monstre, hp_monstre):
+    time.sleep(1)
+    print (f"Un {nom_monstre} apparaît !")
+    time.sleep(1.5)
+    while hp_monstre >0 and hp_joueur>0:
+            print ("Vous avez", hp_joueur, "points de vie.")
+            print (nom_monstre, "a", hp_monstre, "points de vie.")
+            time.sleep(1)
+            #print ("Attaques disponibles :", [spell.__name__ for spell in classes_attackes[classe_joueur.lower()]])
+            print ("Attaques disponibles :")
+
+            for i, spell in enumerate(classes_attackes[classe_joueur.lower()]):
+                print (f"{i+1}. {spell.__name__}")
+
+            choix_attaque = input ("Choisissez votre attaque (1 , 2 , ou 3) : ")
+            if choix_attaque in ["1", "2", "3"]:
+                spell_index = int(choix_attaque) - 1
+                selected_spell = classes_attackes[classe_joueur.lower()][spell_index]
+                hp_monstre = selected_spell(hp_monstre, nom_joueur)
+            else:
+                print ("Choix invalide, essayez de nouveau.")
+                continue
+
+            if hp_monstre <= 0:
+                print (f"Le {nom_monstre} est vaincu !")
+                break
+            elif hp_joueur <=0:
+                 print (f"{nom_joueur} est vaincu ! Game Over.")
+                 break
+            
+            hp_joueur -= poule_attaque(hp_joueur)
+    return hp_joueur
+        
