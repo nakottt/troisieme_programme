4b276195d580bfcc6da228f307c96822e7865af5
Nakottt <hugo.warnie@hotmail.com>
2026-01-11 16:53:24 +0100

    troisieme_programme v0.3

---
+++ spells/mob_spells.py
@@ -0,0 +1,9 @@
+import random
+
+# spell poule sauvage 
+
+def poule_attaque(hp_joueur):
+    degats = random.randint(10,12)
+    hp_joueur -= degats
+    print ("La poule picore et inflige", degats, "points de dégats.")
+    return random.randint(8,12)
---
+++ spells/player_spells.py
@@ -0,0 +1,122 @@
+import random
+
+# spells guerrier
+
+def attaque_auto (hp_monstre, nom_joueur):
+    degats = random.randint(9,11)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+
+def coupret_hit (hp_monstre, nom_joueur):
+    degats = random.randint(10,16)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def execute (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+# spells mage
+
+def boule_de_feu (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def lance_de_glace (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def nova_arcane (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+# spells voleur
+
+def kriss (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def flageler (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def coup_d_epee (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+# spells druide
+
+def colere (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def eclat_lunaire (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def nova_solaire (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+# spells chasseur
+
+def tir_rapide (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def tir_explosif (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre
+
+def morsure_du_serpent (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+# spells pretre
+
+def fouet_mental (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def lumiere_vascillante (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
+
+def jugement (hp_monstre, nom_joueur):
+    degats = random.randint(14,17)
+    hp_monstre -= degats
+    print (nom_joueur, "inflige", degats, "points de dégats.")
+    return hp_monstre 
